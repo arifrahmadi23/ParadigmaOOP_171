266934281fc8830503aec962bb362b4136005362
arifrahmadi23 <muhammad231arif@gmail.com>
2023-05-22 15:24:02 +0700

    membuat class mahasiswa dan membernya

--- ParadigmaOOP_171(1)/ParadigmaOOP_171(1).cpp
+++ ParadigmaOOP_171(1)/ParadigmaOOP_171(1).cpp
@@ -1,3 +1,15 @@
 #include <iosteam>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { // member ini
+
+	}
+};
